dbdd35d1c9d5e648a21bafd0d5c5746bfb8893d6
Nicolas Cavigneaux <nico@bounga.org>
2008-11-04 17:06:12 +0100

    Enhanced command-line modularization

--- lib/command_line.rb
+++ lib/command_line.rb
@@ -1,28 +1,41 @@
 class Renamer
-  module CommandLine
-    def self.feed
+  class CommandLine
+    attr_accessor :action, :arg, :recursive, :overwrite, :opts, :filenames
+    
+    def initialize
+      $VERBOSE, self.recursive, self.overwrite = false
+      self.action, self.arg = nil
+      
+      feed
+      parse
+      process
+    end
+    
+    private
+    
+    def feed
       opts = OptionParser.new
       
       opts.banner = _("Usage: %s [options] FILENAMES") % File.basename($0)
       opts.separator ""
       opts.separator _("Specific options:")
 
-      opts.on("-d", "--downcase", _("Downcase FILENAMES")) { action = :downcase }
-      opts.on("-u", "--upcase", _("Upcase FILENAMES")) { action = :upcase }
-      opts.on("-c", "--capitalize", _("Capitalize FILENAMES")) { action = :capitalize }
-      opts.on("-w", "--word", _("Capitalize each words in FILENAMES")) { action = :word }
-      opts.on("--downcase_ext", _("Downcase FILENAMES extensions")) { action = :downcase_ext }
-      opts.on("--upcase_ext", _("Upcase FILENAMES extensions")) { action = :upcase_ext }
-      opts.on("-s", "--space", _("Replace spaces by underscores in FILENAMES")) { action = :space }
-      opts.on("-U", "--underscore", _("Replace underscores by spaces in FILENAMES")) { action = :underscore }
-      opts.on("--date", _("Add current date to the beginning of FILENAMES")) { action = :date }
-      opts.on("--datetime", _("Add current datetime to the beginning of FILENAMES")) { action = :datetime }
-      opts.on("-b", _("--basename BASENAME"), _("Rename FILENAMES using the specified"), _("BASENAME followed by an incremental number")) { |bn| action = :basename; arg = bn }
-      opts.on("-e", _("--ext NEW"), _("Rename FILENAMES from their actual extension"), _("to the NEW one")) { |new| action = :ext; arg = new }
-      opts.on("-f", "--trim-first n", _("Rename FILENAMES by trimming first n characters")) { |n| action = :trim_first; arg = n }
-      opts.on("-l", "--trim-last n", _("Rename FILENAMES by trimming last n characters")) { |n| action = :trim_last; arg = n }
-      opts.on("-r", "--recursive", _("Run %s recursively across directories") % Const::NAME) { recursive = true }
-      opts.on("-o", "--overwrite", _("Overwrite existing files if needed") % Const::NAME) { overwrite = true }
+      opts.on("-d", "--downcase", _("Downcase FILENAMES")) { self.action = :downcase }
+      opts.on("-u", "--upcase", _("Upcase FILENAMES")) { self.action = :upcase }
+      opts.on("-c", "--capitalize", _("Capitalize FILENAMES")) { self.action = :capitalize }
+      opts.on("-w", "--word", _("Capitalize each words in FILENAMES")) { self.action = :word }
+      opts.on("--downcase_ext", _("Downcase FILENAMES extensions")) { self.action = :downcase_ext }
+      opts.on("--upcase_ext", _("Upcase FILENAMES extensions")) { self.action = :upcase_ext }
+      opts.on("-s", "--space", _("Replace spaces by underscores in FILENAMES")) { self.action = :space }
+      opts.on("-U", "--underscore", _("Replace underscores by spaces in FILENAMES")) { self.action = :underscore }
+      opts.on("--date", _("Add current date to the beginning of FILENAMES")) { self.action = :date }
+      opts.on("--datetime", _("Add current datetime to the beginning of FILENAMES")) { self.action = :datetime }
+      opts.on("-b", _("--basename BASENAME"), _("Rename FILENAMES using the specified"), _("BASENAME followed by an incremental number")) { |bn| self.action = :basename; self.arg = bn }
+      opts.on("-e", _("--ext NEW"), _("Rename FILENAMES from their actual extension"), _("to the NEW one")) { |new| self.action = :ext; self.arg = new }
+      opts.on("-f", "--trim-first n", _("Rename FILENAMES by trimming first n characters")) { |n| self.action = :trim_first; self.arg = n }
+      opts.on("-l", "--trim-last n", _("Rename FILENAMES by trimming last n characters")) { |n| self.action = :trim_last; self.arg = n }
+      opts.on("-r", "--recursive", _("Run %s recursively across directories") % Const::NAME) { self.recursive = true }
+      opts.on("-o", "--overwrite", _("Overwrite existing files if needed") % Const::NAME) { self.overwrite = true }
 
       opts.separator ""
       opts.separator _("Common options:")
@@ -32,22 +45,22 @@ class Renamer
       opts.on("-L", "--license", _("Show some information about %s license") % Const::NAME) { puts Const::LICENSE; exit(0) }
       opts.on("-V", "--verbose", _("Run %s in verbose mode (quiet by default)") % Const::NAME) { $VERBOSE = true }
       
-      return opts
+      self.opts = opts
     end
     
-    def self.parse(opts)
+    def parse
       if ARGV.empty?
-          puts opts.to_s
+          puts self.opts.to_s
           exit(1)
       end
       
-      opts.parse(ARGV)
+      self.filenames = self.opts.parse(ARGV)
     end
     
-    def self.process(action, arg, filenames, recursive, overwrite)
-      r = Renamer.new(filenames, recursive, overwrite)
+    def process
+      r = Renamer.new(self.filenames, self.recursive, self.overwrite)
 
-      case action
+      case self.action
       when :downcase
         r.downcase
       when :upcase
@@ -65,13 +78,13 @@ class Renamer
       when :underscore
         r.underscore_to_space
       when :basename
-        r.basename(arg)
+        r.basename(self.arg)
       when :ext
-        r.ext(arg)
+        r.ext(self.arg)
       when :trim_first
-        r.trim_first(arg)
+        r.trim_first(self.arg)
       when :trim_last
-        r.trim_last(arg)
+        r.trim_last(self.arg)
       when :date
         r.current_date(Time.now)
       when :datetime
